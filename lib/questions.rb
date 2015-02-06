ef5899ef13da6341bdc85ce18947846f6faab807
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-06 14:38:04 +0000

    Gets the first half of a string, makes numbers negative

--- lib/questions.rb
+++ lib/questions.rb
@@ -59,11 +59,14 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  string.length % 2 == 0 ? index = string.length / 2 : index = (string.length + 1) / 2
+  string[0...index]
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  number > 0 ? number * -1 : number
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
