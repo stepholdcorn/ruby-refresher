f9b794fa576e5c4d55b47d1f1151d921db004ee9
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-07 14:38:57 +0000

    Rounds a number up, rounds a number down

--- lib/questions.rb
+++ lib/questions.rb
@@ -168,11 +168,13 @@ end
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+  float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.floor
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -141,12 +141,12 @@ describe 'the Friday test :)' do
     expect(n).to eq 'ello ohnoe'
   end
 
-  xit 'round_up_number' do
+  it 'round_up_number' do
     n = round_up_number 3.142
     expect(n).to eq 4
   end
 
-  xit 'round_down_number' do
+  it 'round_down_number' do
     n = round_down_number 4.9
     expect(n).to eq 4
   end
