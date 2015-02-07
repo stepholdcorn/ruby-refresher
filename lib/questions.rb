3fc347bf0732f6d84eecc5df45082ac17a98a80c
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-07 15:06:05 +0000

    Formats date, gets domain from email address

--- lib/questions.rb
+++ lib/questions.rb
@@ -180,11 +180,14 @@ end
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+  date.strftime('%d/%m/%Y')
 end
 
 # get the domain name *without* the .com part, from an email address
 # so alex@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
+  domain = email.match(/(?:@)(.+?)(?:.com)/).captures
+  domain[0]
 end
 
 # capitalize the first letter in each word of a string,
