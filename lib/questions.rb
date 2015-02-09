33cbfe533020dfff24c096ae2ab851c9c3b1858d
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-09 13:26:07 +0000

    Refactored following code review

--- lib/questions.rb
+++ lib/questions.rb
@@ -11,15 +11,12 @@ end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
-  array.delete(nil)
-  array
+  array.reject{ |element| element == nil }
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
-  array.delete(nil)
-  array.delete(false)
-  array
+  array.reject{ |element| element == nil || element == false }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
@@ -39,8 +36,7 @@ end
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
-  3.times { array.shift }
-  array
+  array.drop(3)
 end
 
 # add an element to the beginning of an array
@@ -60,14 +56,14 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
-  string.length % 2 == 0 ? index = string.length / 2 : index = (string.length + 1) / 2
+  string.length.even? ? index = string.length / 2 : index = (string.length + 1) / 2
   string[0...index]
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
-  number > 0 ? number * -1 : number
+  -number.abs
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of
@@ -75,8 +71,8 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
-  evens = array.select { |element| element % 2 == 0}
-  odds = array.select { |element| element % 2 != 0}
+  evens = array.select { |element| element.even? }
+  odds = array.select { |element| element.odd? }
   [evens, odds]
 end
 
@@ -135,7 +131,7 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
-  Hash[*array.flatten]
+  Hash[*array]
 end
 
 # get all the letters used in an array of words and return
@@ -143,7 +139,7 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
-  array.join('').split('').uniq.sort
+  array.join.split('').uniq.sort
 end
 
 # swap the keys and values in a hash. e.g.
@@ -157,13 +153,13 @@ end
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
-  hash.to_a.inject(:+).inject(:+)
+  hash.flatten.inject(:+)
 end
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
-  string.gsub(/[[:upper:]]+/, '')
+  string.gsub(/[[:upper:]]/, '')
 end
 
 # round up a float up and convert it to an Integer,
@@ -197,10 +193,9 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
-  words = string.split(/\W+/)
-  capitalized_words = words.map {|word| word.length > 3 ? word.capitalize : word.downcase }
+  words = string.capitalize.split(/\W+/)
+  capitalized_words = words.map {|word| word.length > 3 ? word.capitalize : word }
   string = capitalized_words.join(' ')
-  capitalized_string = string.slice(0,1).capitalize + string.slice(1..-1)
 end
 
 # return true if a string contains any special characters
@@ -219,7 +214,7 @@ end
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
-  (range.include? range.end) ? false : true
+  !(range.include? range.end)
 end
 
 # get the square root of a number
